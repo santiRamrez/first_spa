a865623711ec22a6c3c67465fd18ab77f2b7108b
Santiago Ramirez <santi_coding@outlook.com>
2021-07-17 18:41:19 -0400

    Adding webpack rules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import router from "./routes/index";
+/* My style-sheets */
+import "./styles/styles.css";
 
 window.addEventListener("load", router);
 window.addEventListener("hashchange", router);
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 //This plugin is gonna copy our styles to the dist directory
+//To use this you have to put the link tag on the index.html within the public directory
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js", //Entry point
-  //Where to have the result of our js compiled to a simplier version of js
+
   output: {
+    //Where to have the result of our js compiled to a simplier version of js
     path: path.resolve(__dirname, "dist"), //__dirname means the main directory and there it will create dist directory
     filename: "main.js", //Within dist, it will create main.js
   },
@@ -19,11 +23,26 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js?$/, //Rule to use babel
+        test: /\.js?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: "babel-loader", //Rule to use babel
         },
+      },
+      {
+        test: /\.(css|scss)$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], //Rule to use css and sass, you dont need to compile sass anymore
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "static/images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
@@ -33,9 +52,14 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
-
+    new MiniCssExtractPlugin(),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./src/styles/styles.css", to: "" }],
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/img"),
+          to: "assets/img",
+        },
+      ],
     }),
   ],
 };
