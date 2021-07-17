6be9e4f99922e594e9144ed90b7866f5e4be4de8
Santiago Ramirez <santi_coding@outlook.com>
2021-07-17 18:59:40 -0400

    Fixing home direction

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
       <div class="Header-main">
         <div class="Header-logo">
           <h1>
-            <a href="/">Country Data</a>
+            <a href="/first_spa">Country Data</a>
           </h1>
         </div>
         <div class="Header-nav">
